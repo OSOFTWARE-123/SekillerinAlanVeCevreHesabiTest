0b472966454ecaff3fa0fcd09130f00b1751fd25
omers <omers@DESKTOP-HPC4JGB>
2022-06-05 20:30:51 +0300

    Add project files.

---
+++ SekillerinAlanVeCevreHesabiTest/Form1.cs
@@ -0,0 +1,10 @@
+namespace SekillerinAlanVeCevreHesabiTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
